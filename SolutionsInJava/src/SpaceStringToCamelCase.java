5e8833cb5ed07d108a733f52d4cb067d7ad2a801
RaviTeja.Poluri <raviteja.poluri1@gmail.com>
2019-11-04 18:41:05 +0530

    Create SpaceStringToCamelCase.java

---
+++ SolutionsInJava/src/SpaceStringToCamelCase.java
@@ -0,0 +1,27 @@
+    
+    impot java.util.*;
+    
+    /**
+     ** This class will convert space seperated string to camel case
+     ** Ex: "class name is camel case" will be converted to classNameInCamelCase
+    **/
+    public class SpaceStringToCamelCase{
+            public static void main(String[] args) {
+        String name = "class name is camel case";
+        System.out.println(spaceStringToCamelCaseString(name));
+        }
+
+
+    public static String spaceStringToCamelCaseString(String name) {
+        String[] splittedString = name.split(" ");
+        StringBuilder finalString = new StringBuilder(splittedString[0]);
+        for (int i = 1; i < splittedString.length; i++) {
+            String atPosition = splittedString[i];
+            char c = atPosition.charAt(0);
+            String remainingString =
+                new String(new char[]{c}).toUpperCase() + atPosition.substring(1);
+            finalString.append(remainingString);
+        }
+        return finalString.toString();
+    }
+    }
