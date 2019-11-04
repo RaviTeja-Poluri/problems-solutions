38f180b8551de02290e5cc8a35b64aabb063df26
RaviTeja.Poluri <raviteja.poluri1@gmail.com>
2019-11-04 18:57:08 +0530

    Create PermissionsToNumberdValue.java

---
+++ SolutionsInJava/src/PermissionsToNumberdValue.java
@@ -0,0 +1,56 @@
+/**
+*** class will convert given permissions to numbered value.
+*** provided each permission has value defined
+******* w (2)
+******* r (4)
+******* x (1)
+******* - (0)
+*** Ex: "rwxr-x-w-" are provided permissions the resultant value produced will be 752.
+*** so the above is splitted into three parts "rwx","r-x","-w-"
+*** sum of each three is added together to get resultant value
+*** sum of rwx=4+2+1=7
+*** sum of r-x=4+0+1=5
+*** sum of -w-=0+2+0=2
+*** final result is 752
+**/
+
+public class PermissionsToNumberdValue{
+
+      public static void main(String[] args) {
+        String permString = "rwxr-x-w-";
+        int len = permString.length();
+
+        StringBuilder finalOctal = new StringBuilder();
+
+        for (int i = 0; i < len; i += 3) {
+
+            String subString = permString.substring(i, i + 3);
+
+            String first = subString.charAt(0) + "";
+            String second = subString.charAt(1) + "";
+            String third = subString.charAt(2) + "";
+
+            int sum = getNumber(first) + getNumber(second) + getNumber(third);
+
+            finalOctal.append(sum);
+        }
+        System.out.println(finalOctal);
+    }
+
+    private static int getNumber(String val) {
+        switch (val) {
+            case "w":
+                return 2;
+            case "r":
+                return 4;
+            case "x":
+                return 1;
+            case "-":
+                return 0;
+
+        }
+        return 0;
+    }
+
+
+}
